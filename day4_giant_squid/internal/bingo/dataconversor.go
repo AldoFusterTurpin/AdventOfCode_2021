7a15ce7139536c0e2e6bb99889317c2b73dab7cc
Aldo <aldoft@hotmail.com>
2022-03-16 01:10:40 +0100

    refactor: convert some structs functions to regular functions

--- day4_giant_squid/internal/bingo/dataconversor.go
+++ day4_giant_squid/internal/bingo/dataconversor.go
@@ -1,7 +1,9 @@
 package bingo
 
 import (
+	"bufio"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -30,4 +32,59 @@ func ConvertRawInputToBoardsType(rawInput [][]string) []Board {
 		boards = append(boards, board)
 	}
 	return boards
+}
+
+func ConvertSliceStringsToSliceInts(slice []string) []int {
+	var r []int
+	for _, s := range slice {
+		x, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		r = append(r, x)
+	}
+	return r
+}
+
+func GetRawInput(filPath string) (numbersToDraw []string, boards [][]string) {
+	file, err := os.Open(filPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	numbersToDraw = strings.Split(scanner.Text(), ",") // TODO convert numbersToDraw (type string) to type []int
+
+	scanner.Scan()
+	var board []string
+
+	var t string
+	for scanner.Scan() {
+		t = scanner.Text()
+
+		if t == "" {
+
+			// this line is needed to be able to read the whole file even if there are
+			// extra blank lines at the end of the file
+			if len(board) > 0 {
+				boards = append(boards, board)
+			}
+			board = nil
+		} else {
+			board = append(board, strings.TrimSpace(t))
+		}
+	}
+
+	//this line is key to be able to read whole file even if there are
+	// extra blank lines at the end of the file. Boards holds the value of the
+	// last iteration of the for loop
+	if len(board) > 0 {
+		boards = append(boards, board)
+	}
+
+	return numbersToDraw, boards
 }
--- day4_giant_squid/main.go
+++ day4_giant_squid/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"giant_squid/internal/bingo"
-	"log"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
 	fileName := "input_example.txt"
-	numbersToDraw, rawBoards := getRawInput(fileName)
+	numbersToDraw, rawBoards := bingo.GetRawInput(fileName)
 
-	numsToDraw := convertSliceStringsToSliceInts(numbersToDraw)
+	numsToDraw := bingo.ConvertSliceStringsToSliceInts(numbersToDraw)
 	boards := bingo.ConvertRawInputToBoardsType(rawBoards)
 
 	bingoDetector := bingo.NewDetector()
@@ -23,59 +18,4 @@ func main() {
 	bingoWinnerIndex, score := bingoSolver.GetFirstWinnerBoardAndScore(numsToDraw, boards)
 	fmt.Println("bingoWinnerIndex:", bingoWinnerIndex)
 	fmt.Println("score:", score)
-}
-
-func convertSliceStringsToSliceInts(slice []string) []int {
-	var r []int
-	for _, s := range slice {
-		x, err := strconv.Atoi(s)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		r = append(r, x)
-	}
-	return r
-}
-
-func getRawInput(filPath string) (numbersToDraw []string, boards [][]string) {
-	file, err := os.Open(filPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	scanner.Scan()
-	numbersToDraw = strings.Split(scanner.Text(), ",") // TODO convert numbersToDraw (type string) to type []int
-
-	scanner.Scan()
-	var board []string
-
-	var t string
-	for scanner.Scan() {
-		t = scanner.Text()
-
-		if t == "" {
-
-			// this line is needed to be able to read the whole file even if there are
-			// extra blank lines at the end of the file
-			if len(board) > 0 {
-				boards = append(boards, board)
-			}
-			board = nil
-		} else {
-			board = append(board, strings.TrimSpace(t))
-		}
-	}
-
-	//this line is key to be able to read whole file even if there are
-	// extra blank lines at the end of the file. Boards holds the value of the
-	// last iteration of the for loop
-	if len(board) > 0 {
-		boards = append(boards, board)
-	}
-
-	return numbersToDraw, boards
 }
